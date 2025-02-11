bb25462d0f7c747e2eff58575154db88ff25c4ff
runtmeyer <muobotone@gmail.com>
2025-02-11 19:32:46 +0100

    server startup file

--- start.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location $PSScriptRoot
-
-
-venv/Scripts/activate
-uvicorn main:app --host=0.0.0.0 --workers=4
-
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd."$(dirname."$0")"
+source env/bin/activate
+uvicorn main:app --host=0.0.0.0 --workers=4
+
