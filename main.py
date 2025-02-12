d9e8b5fff936f07f60e7d9c687d385199218f240
runtmeyer <muobotone@gmail.com>
2025-02-12 15:26:34 +0100

    deployment test 3

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"testing": "is this still working?"}
 
 @app.get("/healthcheck")
 async def health_check():
