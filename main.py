6d562295eb34df875f29dfb793876c5a5972b503
runtmeyer <muobotone@gmail.com>
2025-02-14 05:53:13 +0100

    fix: mistakenly removed books endpoint

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+app.include_router(api_router, prefix=settings.API_PREFIX)
 @app.get("/healthcheck")
 async def health_check():
     """Checks if server is active."""
