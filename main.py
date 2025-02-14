d56aa09952e15fd86fcc8f935f36df68636d9425
runtmeyer <84744784+runtmeyer@users.noreply.github.com>
2025-02-14 05:44:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ app.add_middleware(
 )
 
 app.include_router(api_router, prefix=settings.API_PREFIX)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 @app.get("/healthcheck")
 async def health_check():
